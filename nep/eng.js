ce75de56c358368bd6ac513cba02fd296595a02c
baralchirag <119888892+baralchirag@users.noreply.github.com>
2024-02-13 11:09:55 +0545

    Add files via upload

---
+++ nep/eng.js
@@ -0,0 +1,24 @@
+var englishDateElement = document.getElementById('englishDate');
+var todayDateElement = document.getElementById('today-date');
+
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var today = new Date();
+var day = today.getDate();
+var monthIndex = today.getMonth(); 
+var year = today.getFullYear();
+var dayIndex = today.getDay();
+
+if (day < 10) {
+    day = '0' + day;
+}
+
+var monthName = monthNames[monthIndex];
+var dayName = dayNames[dayIndex];
+
+var dateString =  day + ' ' + monthName + ' ' + year;
+
+document.getElementById("englishDate").innerHTML = dateString ;
---
+++ nep/layout.js
@@ -0,0 +1,5 @@
+const nepaliDate = new NepaliDate();
+  
+    const formattedNepaliDate = nepaliDate.format("dd mmmm YYYY"); 
+    document.getElementById("nepaliDate").innerHTML =  formattedNepaliDate;
+ 
